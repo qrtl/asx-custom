8b2b27c60624cbfffb6621ea28a6e4f4057a51ab
Yoshi Tashiro <tashiro@quartile.co>
2019-02-06 15:06:33 +0900

    [ADD] product_permission_adj_asx (#1)

---
+++ product_permission_adj_asx/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Product Permission Adjustments",
+    "summary": "",
+    "version": "12.0.1.0.0",
+    "category": "Product",
+    "website": "https://www.quartile.co/",
+    "author": "Quartile Limited",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "purchase",
+        "sale",
+        "stock",
+        "account",
+    ],
+    "data": [
+        "security/ir_model_access.xml",
+    ],
+}

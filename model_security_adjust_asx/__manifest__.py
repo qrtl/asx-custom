ba8e1b986c5c2ae90c62817cc637d40e9d1feaad
Yoshi Tashiro <tashiro@quartile.co>
2019-02-26 18:21:45 +0900

    add model_security_adjust_asx (#4)

---
+++ model_security_adjust_asx/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Model Security Adjust',
+    'version': '11.0.1.0.0',
+    'author': 'Quartile Limited',
+    'website': 'https://www.quartile.co',
+    'category': 'Security',
+    'license': "AGPL-3",
+    'description': "",
+    'depends': [
+        'sale',
+    ],
+    'data': [
+        'security/sale_security.xml',
+    ],
+    'installable': True,
+}

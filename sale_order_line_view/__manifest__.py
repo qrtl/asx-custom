df0567531d21ba36e903d81a61107dd01a204919
Tim <tl@quartile.co>
2019-03-27 19:57:56 +0800

    [ADD] sale_order_line_view (#6)

---
+++ sale_order_line_view/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale Order Line View",
+    "summary": "",
+    "version": "12.0.1.0.0",
+    "category": "Sales",
+    "website": "https://www.quartile.co/",
+    "author": "Quartile Limited",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "views/sale_order_line_views.xml",
+    ],
+}

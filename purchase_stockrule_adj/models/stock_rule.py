03352072e37ccebacc2e9c87e7287dbb2009770d
tslai <tl@quartile.co>
2019-07-23 16:26:46 +0800

    [IMP] Change sale_shipping_id compute and domain logic

---
+++ purchase_stockrule_adj/models/stock_rule.py
@@ -0,0 +1,18 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields, api
+from odoo.exceptions import UserError
+
+
+class StockRule(models.Model):
+    _inherit = 'stock.rule'
+
+    def _make_po_get_domain(self, values, partner):
+        domain = super(StockRule, self)._make_po_get_domain(values, partner)
+        if 'group_id' in values and values['group_id'].sale_id:
+            domain += (
+                ('sale_shipping_id', '=',
+                 values['group_id'].sale_id.partner_shipping_id.id),
+            )
+        return domain

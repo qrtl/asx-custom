01a57ce55980af2d507dfdddbf6c778d0c372017
Tim <tl@quartile.co>
2019-07-23 16:38:32 +0800

    [ADD] purchase_stockrule_adj (#13)

---
+++ purchase_stockrule_adj/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Purchase Stock Rule Adjustments",
+    "summary": "",
+    "version": "12.0.1.0.0",
+    "category": "Sale & Purchase",
+    "website": "https://www.quartile.co/",
+    "author": "Quartile Limited",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "purchase_stock",
+    ],
+    "data": [
+        "views/purchase_order_views.xml",
+    ],
+}

d466d99d3f37501cc2d6afc40d94750512fbb347
Yoshi Tashiro <tashiro@quartile.co>
2019-08-01 15:59:06 +0900

    add account_invoice_report_vat (#14)

---
+++ account_invoice_report_vat/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Account Invoice Report VAT",
+    "summary": "",
+    "description": """
+Shows ship-to party's VAT number in printed invoice. 
+    """,
+    "version": "12.0.1.0.0",
+    "category": "Accounting",
+    "website": "https://www.quartile.co/",
+    "author": "Quartile Limited",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "views/invoice_report_templates.xml",
+    ],
+}

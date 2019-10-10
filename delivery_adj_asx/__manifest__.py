895d0152130244528a3b136d7eb263f72d2f773a
Sokream Phan <sokream@quartile.co>
2019-10-10 16:07:11 +0800

    [ADD] delivery_adj_asx module

---
+++ delivery_adj_asx/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Delivery Adjust",
+    "summary": "",
+    "description": """
+ - Avoid create the delivery charge line in sales order for free shipping. 
+    """,
+    'version': '12.0.1.0.0',
+    "category": "Stock",
+    "website": "https://www.quartile.co/",
+    "author": "Quartile Limited",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "delivery"
+    ],
+}

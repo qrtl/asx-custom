d776bace8cf421380ce7562ea5f8f9b849f22fa6
tslai <tl@quartile.co>
2019-10-10 16:43:07 +0800

    [ADD] sale_delivery_client_order_ref

---
+++ sale_delivery_client_order_ref/models/stock_picking.py
@@ -0,0 +1,13 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields
+
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    client_order_ref = fields.Char(
+        related='group_id.sale_id.client_order_ref',
+        string='Customer Reference',
+    )

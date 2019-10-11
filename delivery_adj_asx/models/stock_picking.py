44b4264dd06874f9d7fd2e9b7df5937446fe253a
SokreamPhanqrl <56345535+SokreamPhanqrl@users.noreply.github.com>
2019-10-11 10:55:55 +0700

    [ADD] delivery_adj_asx module (#21)

---
+++ delivery_adj_asx/models/stock_picking.py
@@ -0,0 +1,21 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, api
+
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    @api.multi
+    def _add_delivery_cost_to_so(self):
+        """ Override standard method so that sales order line is not created when
+            carrier_price is zero.
+        """
+        self.ensure_one()
+        sale_order = self.sale_id
+        if sale_order.invoice_shipping_on_delivery:
+            # sale_order._create_delivery_line(self.carrier_id, self.carrier_price)
+            if self.carrier_price:
+                sale_order._create_delivery_line(self.carrier_id,
+                                                 self.carrier_price)

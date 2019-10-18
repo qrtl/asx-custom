2a729239e7fef64038188b556bf186fc7f8b953d
Sokream Phan <sokream@quartile.co>
2019-10-18 09:51:35 +0800

    504-add-sale_order_invoicing_policy

---
+++ sale_order_invoicing_policy/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale Order Invoice Policy",
+    "summary": "",
+    "description": """
+This moudle allow user to change Invoicing Policy on sale order.
+""",
+    "version": "12.0.1.0.0",
+    "category": "Sale,
+    "website": "https://www.quartile.co",
+    "author": "Quartile Limited",
+    "license": "AGPL-3",
+    "depends": [
+        "sale_stock",
+    ],
+    "data": [
+        "views/sale_order_views.xml",
+    ],
+    "installable": True,
+}
+

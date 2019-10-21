1ef08a5fd5efb27d03bd5448c5495d7de6059567
SokreamPhanqrl <56345535+SokreamPhanqrl@users.noreply.github.com>
2019-10-21 11:11:31 +0700

    Add sale_order_invoicing_policy (#23)
    * Add sale_order_invoicing_policy
    
    * change coding style
    
    * change field name of sale_order_invoice_policy to invoice_policy

---
+++ sale_order_invoicing_policy/models/sale_order.py
@@ -0,0 +1,14 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields, api
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    invoice_policy = fields.Selection(
+        [('order', 'Ordered quantities'),
+         ('delivery', 'Delivered quantities')],
+        string='Invoicing Policy',
+    )

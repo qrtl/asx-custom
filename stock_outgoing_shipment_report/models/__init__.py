60cd8ef2be96ec6b6003f8d6fc758278baf173ae
Tim <tl@quartile.co>
2019-10-29 17:48:20 +0800

    [IMP] Add delivery account model, remove consignee code (#26)

--- stock_outgoing_shipment_report/models/__init__.py
+++ stock_outgoing_shipment_report/models/__init__.py
@@ -1,6 +1,6 @@
+from . import delivery_carrier_account
 from . import delivery_carrier_service
 from . import delivery_carrier
-from . import delivery_consignee_code
 from . import product_template
 from . import res_partner
 from . import sale_order

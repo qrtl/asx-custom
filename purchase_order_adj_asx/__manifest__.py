07b629db84d10ce5fa674d60f30a6dba6e6a3edc
SokreamPhanqrl <56345535+SokreamPhanqrl@users.noreply.github.com>
2019-11-05 21:30:58 +0700

    add memo,evidence_status field to PO (#35)
    * add memo,evidence_status field to PO
    
    * adjust field evidence_status position
    
    * add action for attachment view
    
    * remove string from field

---
+++ purchase_order_adj_asx/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Purchase Order Adjustments",
+    "summary": "",
+    "version": "12.0.1.0.0",
+    "category": "Purchase",
+    "website": "https://www.quartile.co/",
+    "author": "Quartile Limited",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "purchase",
+        "mail",
+    ],
+    "data": [
+        "views/purchase_order_views.xml",
+    ],
+}
---
+++ purchase_order_adj_asx/models/purchase_order.py
@@ -0,0 +1,24 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields, api
+
+
+class PurchaseOrder(models.Model):
+   _inherit = 'purchase.order'
+
+   evidence_status = fields.Selection([
+      ('more', 'Need More'),
+      ('done', 'Done'),
+   ], string="Evidence Status", default='')
+    
+   evidence_text = fields.Char()
+   memo = fields.Text()
+
+   @api.multi
+   def action_get_attachment_view(self):
+      self.ensure_one()
+      res = self.env['ir.actions.act_window'].for_xml_id('base', 'action_attachment')
+      res['domain'] = [('res_model', '=', 'purchase.order'), ('res_id', 'in', self.ids)]
+      res['context'] = {'default_res_model': 'purchase.order', 'default_res_id': self.id}
+      return res

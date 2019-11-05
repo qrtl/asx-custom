5801eee649f1f511560176b91c8b9acf6f5dc30f
Sokream Phan <sokream@quartile.co>
2019-11-05 16:31:29 +0800

    add action for attachment view

--- purchase_order_adj_asx/models/purchase_order.py
+++ purchase_order_adj_asx/models/purchase_order.py
@@ -5,12 +5,20 @@ from odoo import models, fields, api
 
 
 class PurchaseOrder(models.Model):
-    _inherit = 'purchase.order'
+   _inherit = 'purchase.order'
 
-    evidence_status = fields.Selection([
-       ('more', 'Need More'),
-       ('done', 'Done'),
-    ], string="Evidence Status", default='')
+   evidence_status = fields.Selection([
+      ('more', 'Need More'),
+      ('done', 'Done'),
+   ], string="Evidence Status", default='')
     
-    evidence_text = fields.Char()
-    memo = fields.Text(string="Memo")
+   evidence_text = fields.Char()
+   memo = fields.Text(string="Memo")
+
+   @api.multi
+   def action_get_attachment_view(self):
+      self.ensure_one()
+      res = self.env['ir.actions.act_window'].for_xml_id('base', 'action_attachment')
+      res['domain'] = [('res_model', '=', 'purchase.order'), ('res_id', 'in', self.ids)]
+      res['context'] = {'default_res_model': 'purchase.order', 'default_res_id': self.id}
+      return res

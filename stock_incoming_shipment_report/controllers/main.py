ce27a27d522f5e135b3fcb619b5ef911cdfee792
Tim <tl@quartile.co>
2019-11-07 15:24:29 +0800

    [ADD] stock_incoming_shipment_report (#44)

---
+++ stock_incoming_shipment_report/controllers/main.py
@@ -0,0 +1,36 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+import json
+from odoo.addons.web.controllers.main import CSVExport
+from odoo.addons.web.controllers.main import ExcelExport
+
+
+class CSVExportInherit(CSVExport):
+
+    # Override the base() method which is used to gather fields' values in the
+    # export CSV.
+    def base(self, data, token):
+        params = json.loads(data)
+        # When the stock.incoming.shipment.report model is selected, remove
+        # the first element in the fields list which is the External ID by
+        # default.
+        if params['model'] == 'stock.incoming.shipment.report':
+            params['fields'].pop(0)
+            data = json.dumps(params)
+        return super(CSVExportInherit, self).base(data, token)
+
+
+class ExcelExportInherit(ExcelExport):
+
+    # Override the base() method which is used to gather fields' values in the
+    # export Excel.
+    def base(self, data, token):
+        params = json.loads(data)
+        # When the stock.incoming.shipment.report model is selected, remove
+        # the first element in the fields list which is the External ID by
+        # default.
+        if params['model'] == 'stock.incoming.shipment.report':
+            params['fields'].pop(0)
+            data = json.dumps(params)
+        return super(ExcelExportInherit, self).base(data, token)
---
+++ stock_incoming_shipment_report/wizard/stock_incoming_shipment_report_wizard.py
@@ -0,0 +1,27 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields, api
+
+
+class StockIncomingShipmentReportWizard(models.TransientModel):
+    _name = 'stock.incoming.shipment.report.wizard'
+
+    date_expected = fields.Datetime('Expected Date')
+
+    @api.multi
+    def generate_stock_incoming_shipment_report_action(self):
+        self.ensure_one()
+        picking_ids = self.env.context.get('picking_ids', [])
+        moves = self.env['stock.picking'].browse(
+            picking_ids).mapped('move_lines').filtered(lambda l: l.date_expected <= self.date_expected)
+        self._cr.execute("DELETE FROM stock_incoming_shipment_report")
+        for move in moves:
+            vals = {
+                'move_id': move.id,
+                'tracking_pro_num': move.picking_id.carrier_tracking_ref or False,
+                'anticipated_arrival_date_edit': move.date_expected,
+                'carrier_code': move.picking_id.carrier_id and move.picking_id.carrier_id.name or False,
+            }
+            self.env['stock.incoming.shipment.report'].create(vals)
+        return self.env.ref('stock_incoming_shipment_report.action_stock_incoming_shipment_report').read()[0]

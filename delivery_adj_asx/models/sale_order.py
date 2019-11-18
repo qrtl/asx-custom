dc18e62de8ee931c8b8e38c23ed96b04376a51ee
Tim <tl@quartile.co>
2019-11-18 13:08:39 +0800

    [IMP] Add salesperson to delivery when confirmation (#47)

---
+++ delivery_adj_asx/models/sale_order.py
@@ -0,0 +1,16 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, api
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.multi
+    def _action_confirm(self):
+        res = super(SaleOrder, self)._action_confirm()
+        for order in self:
+            order.mapped('picking_ids').message_subscribe(
+                partner_ids=[order.user_id.partner_id.id])
+        return res

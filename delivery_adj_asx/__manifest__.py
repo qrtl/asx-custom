bd29825a56cf19d54aeb47c4f51d4a07f8d96378
Tim <tl@quartile.co>
2019-11-18 13:08:39 +0800

    [IMP] Add salesperson to delivery when confirmation (#47)

--- delivery_adj_asx/__manifest__.py
+++ delivery_adj_asx/__manifest__.py
@@ -1,18 +1,19 @@
 # Copyright 2019 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    "name": "Delivery Adjust",
+    "name": "Delivery Functions Adjustments",
     "summary": "",
     "description": """
- - Avoid create the delivery charge line in sales order for free shipping. 
+ - Avoid create the delivery charge line in sales order for free shipping.
+ - Add Sale Order Salesperon as outgoing shipment's follower while confirmation.
     """,
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.1.0',
     "category": "Stock",
     "website": "https://www.quartile.co/",
     "author": "Quartile Limited",
     "license": "AGPL-3",
     "installable": True,
     "depends": [
-        "delivery"
+        "delivery",
     ],
 }

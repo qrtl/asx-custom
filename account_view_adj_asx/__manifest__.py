87ca62bfaf3bbb384526bc05db6fcf6ddc1e39bf
Sokream Phan <sokream@quartile.co>
2019-11-19 16:23:51 +0800

    adjust version

--- account_view_adj_asx/__manifest__.py
+++ account_view_adj_asx/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Accounting Views Adjustments",
     "summary": "",
-    "version": "12.0.2.0.0",
+    "version": "12.0.1.1.0",
     "category": "Accounting",
     "website": "https://www.quartile.co/",
     "author": "Quartile Limited",

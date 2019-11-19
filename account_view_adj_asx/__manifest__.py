f4a9d3d64d2fd2f4966fbc11776c1a71647e2e38
SokreamPhanqrl <56345535+SokreamPhanqrl@users.noreply.github.com>
2019-11-19 15:31:24 +0700

    [ADD]communication field to payment tree view (#50)
    * [ADD]communication field to payment tree view
    
    * adjust version

--- account_view_adj_asx/__manifest__.py
+++ account_view_adj_asx/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Accounting Views Adjustments",
     "summary": "",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.1.0",
     "category": "Accounting",
     "website": "https://www.quartile.co/",
     "author": "Quartile Limited",
@@ -14,5 +14,6 @@
     ],
     "data": [
         "views/account_invoice_views.xml",
+        "views/account_payment_view.xml",
     ],
 }

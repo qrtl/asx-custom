48ecf6a71db209ad7afbcd657e35614cbbf68c67
Yoshi Tashiro <tashiro@quartile.co>
2019-12-24 21:48:27 +0900

    676 fix account view adj asx (#53)
    * fix adjustment on invoice form
    
    * black

--- account_view_adj_asx/__manifest__.py
+++ account_view_adj_asx/__manifest__.py
@@ -3,17 +3,12 @@
 {
     "name": "Accounting Views Adjustments",
     "summary": "",
-    "version": "12.0.1.1.0",
+    "version": "12.0.1.1.1",
     "category": "Accounting",
     "website": "https://www.quartile.co/",
     "author": "Quartile Limited",
     "license": "AGPL-3",
     "installable": True,
-    "depends": [
-        "account",
-    ],
-    "data": [
-        "views/account_invoice_views.xml",
-        "views/account_payment_view.xml",
-    ],
+    "depends": ["account"],
+    "data": ["views/account_invoice_views.xml", "views/account_payment_view.xml"],
 }

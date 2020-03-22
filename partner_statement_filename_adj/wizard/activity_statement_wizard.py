02ef0f0e4facabe39e3c62693e85c07fe594825a
Yoshi Tashiro <tashiro@quartile.co>
2020-03-22 14:59:22 +0800

    add comment [skip ci]

--- partner_statement_filename_adj/wizard/activity_statement_wizard.py
+++ partner_statement_filename_adj/wizard/activity_statement_wizard.py
@@ -8,6 +8,13 @@ class ActivityStatementWizard(models.TransientModel):
     _inherit = "activity.statement.wizard"
 
     def _export(self):
+        """ Adjustment on print_report_name of the report does not work when
+        docids are not passed to report_download method of
+        /web/controllers/main.py, which is the case when the report is printed
+        via wizard. Therefore we rely on this rather hacky way of updating
+        the name of the report (which is used as the download file name) to
+        handle the requirement.
+        """
         report = self.env.ref("partner_statement.action_print_activity_statement")
         report_name = "Activity Statement "
         str_today = fields.Date.to_string(fields.Date.context_today(self))

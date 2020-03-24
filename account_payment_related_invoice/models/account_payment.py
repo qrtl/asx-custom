90b7a9c5853454227d9f2aa04938c313fec0211e
tslai <tl@quartile.co>
2020-03-24 17:49:59 +0800

    [ADD] account_payment_related_invoice

---
+++ account_payment_related_invoice/models/account_payment.py
@@ -0,0 +1,20 @@
+# Copyright 2020 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class AccountPayment(models.Model):
+    _inherit = "account.payment"
+
+    related_invoice_number = fields.Char(
+        string="Invoice(s)",
+        compute="_compute_related_invoice_number",
+    )
+
+    @api.multi
+    def _compute_related_invoice_number(self):
+        for payment in self:
+            if payment.invoice_ids:
+                payment.related_invoice_number = ','.join(
+                    payment.invoice_ids.mapped('number'))

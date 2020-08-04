a26dc6a6823caa57eb1b529d5b1b1f532d158483
Tim <tl@quartile.co>
2020-08-04 17:27:43 +0800

    [1140][IMP] Add picking_partner_id to stock move line (#66)

---
+++ stock_move_line_partner/models/stock_move_line.py
@@ -0,0 +1,12 @@
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import fields, models
+
+
+class StockMoveLine(models.Model):
+    _inherit = "stock.move.line"
+
+    picking_partner_id = fields.Many2one(
+        "res.partner", "Transfer Destination Address", related="picking_id.partner_id",
+    )
---
+++ stock_move_view_adj/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module does the following:
+
+* Adjust stock move views.

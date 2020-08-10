5e5c8dbfa61602cfc45dcd12a3670e321afe4385
Yogesh Mahera <yogesh.mahera.serpentcs@gmail.com>
2020-08-10 12:58:36 +0530

    [IMP]Regenerate README.rst, Fix Black

--- account_financial_report_extended/__manifest__.py
+++ account_financial_report_extended/__manifest__.py
@@ -1,5 +1,5 @@
 # Copyright 2020 Quartile Limited
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Account Financial Reports Extended",
     "version": "12.0.1.0.0",
@@ -7,7 +7,7 @@
     "category": "Stock",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co/",
-    "license": "LGPL-3",
+    "license": "AGPL-3",
     "depends": ["account_financial_report"],
     "data": ["wizard/general_ledger_wizard_view.xml"],
     "installable": True,
--- account_financial_report_extended/wizard/general_ledger_wizard.py
+++ account_financial_report_extended/wizard/general_ledger_wizard.py
@@ -1,5 +1,5 @@
 # Copyright 2020 Quartile Limited
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
 

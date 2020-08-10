4af55cf5f72d21596bd1d17c51d6d5a3eb436fe0
Yogesh Mahera <yogesh.mahera.serpentcs@gmail.com>
2020-08-10 09:52:16 +0530

    [Add] Added Test-cases

--- sale_invoice_enduser/tests/test_sale_order.py
+++ sale_invoice_enduser/tests/test_sale_order.py
@@ -8,34 +8,41 @@ class TestSaleOrder(TransactionCase):
     def setUp(self):
         super(TestSaleOrder, self).setUp()
         self.partner = self.env.ref("base.res_partner_2")
-        self.product1 = self.env['product.product'].create({
-            'name': 'Test Product',
-            'type': 'service'
-        })
-        self.enduser = self.env['res.partner'].create({
-            'name': 'Test Enduser'
-        })
+        self.product1 = self.env["product.product"].create(
+            {"name": "Test Product", "type": "service"}
+        )
+        self.enduser = self.env["res.partner"].create({"name": "Test Enduser"})
 
     def test_prepare_invoice(self):
         # Create the SO with four order lines
-        self.sale_order = self.env['sale.order'].create({
-            'partner_id': self.partner.id,
-            'enduser_id': self.enduser.id,
-            'order_line': [(0, 0, {
-                'product_id': self.product1.id,
-                'product_uom_qty': 1,
-                'price_unit': 100,
-            })],
-        })
+        self.sale_order = self.env["sale.order"].create(
+            {
+                "partner_id": self.partner.id,
+                "enduser_id": self.enduser.id,
+                "order_line": [
+                    (
+                        0,
+                        0,
+                        {
+                            "product_id": self.product1.id,
+                            "product_uom_qty": 1,
+                            "price_unit": 100,
+                        },
+                    )
+                ],
+            }
+        )
         self.sale_order.action_confirm()
 
         # Create the invoice
-        self.sale_order.action_invoice_create(final=True)
+        self.sale_order.action_invoice_create()
+        self.invoice = self.sale_order.invoice_ids
 
         # compare the `enduser_id` field data with invoice Enduser field
         # the values passing through `_prepare_invoice` method to invoice.
         self.assertEqual(
-            self.sale_order.invoice_ids[0].enduser_id,
+            self.invoice.enduser_id,
             self.enduser,
-            'End User field from invoice does not'
-            ' matching with test records Enduser field')
+            "End User field from invoice does not"
+            " matching with test records Enduser field",
+        )

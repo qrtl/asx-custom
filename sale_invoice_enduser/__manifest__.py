e85d1a23128a1694455cf5c69f07277c0be5b0df
Yogesh Mahera <yogesh.mahera.serpentcs@gmail.com>
2020-08-18 15:15:29 +0530

    [1139][ADD] sale_invoice_enduser (#68)

---
+++ sale_invoice_enduser/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Sale Invoice Enduser",
+    "summary": "",
+    "version": "12.0.1.0.0",
+    "category": "Sales",
+    "website": "https://www.quartile.co/",
+    "author": "Quartile Limited",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": ["sale"],
+    "data": ["views/sale_order_views.xml", "views/account_invoice_views.xml"],
+}
---
+++ sale_invoice_enduser/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module does the following:
+
+* Adds EndUser field to sales order and invoice.

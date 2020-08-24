22934cd1e4e0a572a6896dffd7829f188a3dcca0
Tim <tl@quartile.co>
2020-08-24 16:06:11 +0800

    [1120][ADD] stock_inventory_accounting_date_adj (#72)

---
+++ stock_inventory_accounting_date_adj/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Stock Inventory Accounting Date Adjustment",
+    "summary": "",
+    "version": "12.0.1.0.0",
+    "category": "Inventory",
+    "website": "https://www.quartile.co/",
+    "author": "Quartile Limited",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": ["stock_account"],
+    "data": ["views/stock_inventory_views.xml"],
+}
---
+++ stock_inventory_accounting_date_adj/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module does the following:
+
+- Removes the groups attribute of accounting_date in the stock inventory form view.

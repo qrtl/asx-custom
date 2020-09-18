35bf9a406ef5c210bc98c4630394f4a902a328b0
Yoshi Tashiro <tashiro@quartile.co>
2020-09-18 09:36:54 +0800

    [ADD] account_financial_risk_ext

---
+++ account_financial_risk_ext/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Account Financial Risk Ext',
+    'version': '12.0.1.0.0',
+    'category': 'Accounting',
+    'license': 'AGPL-3',
+    'author': 'Quartile Limited',
+    'website': 'https://www.quartile.co',
+    'depends': [
+        'account_financial_risk',
+    ],
+    'data': [
+        'views/res_partner_view.xml',
+    ],
+    'installable': True,
+}
---
+++ account_financial_risk_ext/models/res_partner.py
@@ -0,0 +1,17 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    credit_balance = fields.Monetary(compute="_compute_risk_exception")
+
+    @api.multi
+    @api.depends(lambda x: x._get_depends_compute_risk_exception())
+    def _compute_risk_exception(self):
+        super()._compute_risk_exception()
+        for partner in self.filtered('customer'):
+            partner.credit_balance = partner.credit_limit - partner.risk_total

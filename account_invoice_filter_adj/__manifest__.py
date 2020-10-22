493a28da08f2ec5b4629cf11dfb3e286b714b369
Ryoko <ryoko@quartile.co>
2020-10-22 16:58:26 +0800

    [IMP]add invoice date filter

---
+++ account_invoice_filter_adj/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Invoice Views Filter Adjustments",
+    "summary": "",
+    "version": "12.0.1.0.1",
+    "category": "Accounting",
+    "website": "https://www.quartile.co/",
+    "author": "Quartile Limited",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["account"],
+    "data": ["views/account_invoice_views.xml", "views/account_invoice_report_view.xml"],
+}

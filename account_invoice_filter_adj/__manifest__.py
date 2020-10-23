de463cb6df70ce8f85a4380305deeb1d70fc986a
Ryoko Tsuda <70553601+Ryoko04@users.noreply.github.com>
2020-10-23 14:39:26 +0900

    [1292][IMP]add invoice date filter (#76)
    * [IMP]add invoice date filter
    
    * [ADD] Black, isort
    
    * [UPD] license info
    
    Co-authored-by: Yoshi Tashiro <tashiro@quartile.co>
    
    * [UPD] license info 2
    
    Co-authored-by: Yoshi Tashiro <tashiro@quartile.co>
    
    Co-authored-by: Yoshi Tashiro <tashiro@quartile.co>

---
+++ account_invoice_filter_adj/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Invoice Views Filter Adjustments",
+    "summary": "",
+    "version": "12.0.1.0.1",
+    "category": "Accounting",
+    "website": "https://www.quartile.co/",
+    "author": "Quartile Limited",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": ["account"],
+    "data": [
+        "views/account_invoice_views.xml",
+        "views/account_invoice_report_view.xml",
+    ],
+}

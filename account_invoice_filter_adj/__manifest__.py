05b8693d159cf13fcce5bc54f6be4bc2a2b331b9
Ryoko Tsuda <70553601+Ryoko04@users.noreply.github.com>
2020-10-23 14:22:55 +0900

    [UPD] license info 2
    Co-authored-by: Yoshi Tashiro <tashiro@quartile.co>

--- account_invoice_filter_adj/__manifest__.py
+++ account_invoice_filter_adj/__manifest__.py
@@ -1,5 +1,5 @@
-# Copyright 2019 Quartile Limited
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Invoice Views Filter Adjustments",
     "summary": "",

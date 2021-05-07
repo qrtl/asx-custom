89b0667d0aaa3b7e4715bb5b9a87a9fcaabd5908
Yoshi Tashiro <tashiro@quartile.co>
2021-05-07 22:05:10 +0800

    adjust views, update readme

--- sale_tier_validation_quotation_print/__manifest__.py
+++ sale_tier_validation_quotation_print/__manifest__.py
@@ -1,13 +1,14 @@
 # Copyright 2019 Open Source Integrators
+# Copyright 2021 Quartile Limited
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    "name": "Sale Tier Validation",
+    "name": "Sale Tier Validation Quotation Print",
     "summary": "Extends the functionality of Sale Orders to "
                "support a tier validation process.",
     "version": "12.0.1.0.0",
     "category": "Sale",
-    "website": "https://github.com/OCA/sale-workflow",
-    "author": "Open Source Integrators, Odoo Community Association (OCA)",
+    "website": "https://quartile.co",
+    "author": "Open Source Integrators, Odoo Community Association (OCA), Quartile Limited",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
@@ -16,6 +17,7 @@
         "base_tier_validation",
     ],
     "data": [
+        "report/sale_report.xml",
         "views/sale_order_view.xml",
     ],
 }

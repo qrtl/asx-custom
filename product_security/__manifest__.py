6a87192939d57eaa835cf1bae68c888a751d9262
Aungkokolin1997 <lin@quartile.co>
2023-10-17 11:16:57 +0630

    [ADD] product_security

---
+++ product_security/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 Quartile Limited
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Product Security",
+    "summary": "",
+    "version": "12.0.1.0.0",
+    "category": "Product",
+    "website": "https://www.quartile.co",
+    "author": "Quartile Limited",
+    "license": "AGPL-3",
+    "depends": ["product"],
+    "data": [
+        "security/product_security.xml",
+        "views/product_template_views.xml"
+    ],
+    "installable": True,
+}
---
+++ product_security/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+This module restricts the updating of standard price for products to only those users
+in the group_product_manager group.
